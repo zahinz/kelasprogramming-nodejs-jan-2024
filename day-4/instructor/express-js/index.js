a925cde3373146c1de57cbbc5bd4bc14b6cf4263
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-03-21 00:01:56 +0800

    finish bmi calculator express js

--- day-4/instructor/express-js/index.js
+++ day-4/instructor/express-js/index.js
@@ -4,26 +4,65 @@ const express = require("express");
 const app = express();
 const PORT = 8989;
 
+// initialise static folder - css, js, images
+app.use(express.static("public"));
+
+// initialise body-parser
+// why do we need body-parser?
+// body-parser is a middleware that helps to parse the body from http request and make it available under req.body as a javascript object
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // GET request
 app.get("/", function (req, res) {
   const homePagePath = path.join(__dirname, "pages", "index.html");
   const homePage = fs.readFileSync(homePagePath, "utf-8");
   res.setHeader("Content-Type", "text/html");
-  res.send(homePage);
+  res.status(200).send(homePage);
 });
 
 app.get("/bmi-form", function (req, res) {
   const bmiFormPath = path.join(__dirname, "pages", "bmi-form.html");
   const bmiForm = fs.readFileSync(bmiFormPath, "utf-8");
   res.setHeader("Content-Type", "text/html");
-  res.send(bmiForm);
+  res.status(200).send(bmiForm);
 });
 
 app.get("/bmi-result", function (req, res) {
+  const bmi = req.query.bmi;
+  console.log(bmi);
   const bmiResultPath = path.join(__dirname, "pages", "bmi-result.html");
-  const bmiResult = fs.readFileSync(bmiResultPath, "utf-8");
+  let bmiResult = fs.readFileSync(bmiResultPath, "utf-8");
+  // bmiResult is a string
+  // replace the placeholder with the actual value
+  if (bmi) {
+    bmiResult = bmiResult.replace("[[BMI-RESULT]]", bmi);
+  } else {
+    bmiResult = bmiResult.replace("[[BMI-RESULT]]", "No BMI value");
+  }
+
   res.setHeader("Content-Type", "text/html");
-  res.send(bmiResult);
+  res.status(200).send(bmiResult);
+});
+
+// POST request from BMI form
+app.post("/calculate", function (req, res) {
+  // view the data from the form inputs
+  const data = req.body;
+  console.log(data);
+  const weight = Number(data.weight);
+  const height = Number(data.height);
+  const bmi = (weight / (height * height)).toFixed(2);
+  console.log(bmi);
+  res.redirect("/bmi-result" + "?bmi=" + bmi);
+});
+
+// not found page
+app.use(function (req, res) {
+  const notFoundPath = path.join(__dirname, "pages", "404.html");
+  const notFound = fs.readFileSync(notFoundPath, "utf-8");
+  res.setHeader("Content-Type", "text/html");
+  res.status(404).send(notFound);
 });
 
 app.listen(PORT, function () {
---
+++ day-4/instructor/express-js/public/script.js
@@ -0,0 +1,1 @@
+console.log("client side script is running...");
