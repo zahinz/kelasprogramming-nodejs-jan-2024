b786128922fe6a96903277fc12e8032c2010a026
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-04-02 23:43:43 +0800

    Add pg dependency and database connection

---
+++ day-7/instructor/rest-to-do/database/connection.js
@@ -0,0 +1,41 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: "127.0.0.1",
+  user: "zahin",
+  password: "",
+  database: "to-do-app-april-2024",
+  //
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export async function testConnection() {
+  // try catch block
+  try {
+    //   promise is pending
+    const dbName = await pool.query("SELECT current_database()");
+    const dbRes = await pool.query("SELECT NOW()");
+    const time = dbRes.rows[0].now;
+    const name = dbName.rows[0].current_database;
+    //   promise is fullfilled
+    console.log(`Connected to ${name} at ${time}`);
+  } catch (error) {
+    //   promise is rejected
+    console.error(error);
+    console.error("Database connection failed");
+  }
+}
+
+// PROMISE 101
+// promise is variable for future value
+// since promise is a variable for future value, we need to wait for the value to be available by using asyncronous function
+// WAITING / PENUGGUAN
+// state of promise - pending, fulfilled, rejected
+// fullfilled - value is available / database query is successful
+// rejected - value is not available / database query is failed / error
+// try-catch block
+// try - block of code pending and resolved
+// catch - block of code rejected
--- day-7/instructor/rest-to-do/index.js
+++ day-7/instructor/rest-to-do/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import healthController from "./controller/health.js";
 import notFound from "./controller/not-found.js";
+import { testConnection } from "./database/connection.js";
 
 const app = express();
 const PORT = 8787;
+
+// middleware
+// parse application/json and application/x-www-form-urlencoded to req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// database connection
+testConnection();
 
 // GET and POST health routes
 app.get("/", healthController.get);
