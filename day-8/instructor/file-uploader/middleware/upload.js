2fa88f38723df192e607273030d4674e6533a40d
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2024-04-17 22:47:16 +0800

    Update file uploader functionality and dependencies

--- day-8/instructor/file-uploader/middleware/upload.js
+++ day-8/instructor/file-uploader/middleware/upload.js
@@ -1,8 +1,23 @@
 import multer from "multer";
+import path from "path";
+import crypto from "crypto";
 
-const upload = multer({
-  // configure the destination folder for the uploaded files
-  dest: "uploads/",
+// const upload = multer({
+//   // configure the destination folder for the uploaded files
+//   dest: "uploads/",
+// });
+
+const uuid = crypto.randomUUID();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuid + path.extname(file.originalname));
+  },
 });
+
+const upload = multer({ storage: storage });
 
 export default upload;
